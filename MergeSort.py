7ed47e0f9190ecce41589ad724f8c428be364d13
Christopher Himes <cshimes@gmail.com>
2015-03-21 13:25:32 -0400

    merge sort added

---
+++ MergeSort.py
@@ -0,0 +1,32 @@
+# Merge Sort
+# <= 6n*log2(n)+6n
+
+def merge_sort(items):
+    if len(items) > 1:
+        mid = len(items) / 2        # Determine the midpoint and split
+        left = items[0:mid]
+        right = items[mid:]
+
+        merge_sort(left)            # Sort left list in-place
+        merge_sort(right)           # Sort right list in-place
+
+        l, r = 0, 0
+        for i in range(len(items)):     # Merging the left and right list
+
+            lval = left[l] if l < len(left) else None
+            rval = right[r] if r < len(right) else None
+
+            if (lval and rval and lval < rval) or rval is None:
+                items[i] = lval
+                l += 1
+            elif (lval and rval and lval >= rval) or lval is None:
+                items[i] = rval
+                r += 1
+            else:
+                raise Exception('Could not merge, sub arrays sizes do not match the main array')
+
+
+numbers_to_sort = [20, 5, 10, 11, 3, 1, 7]
+print numbers_to_sort
+merge_sort(numbers_to_sort)
+print numbers_to_sort
